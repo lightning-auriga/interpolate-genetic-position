8f04ff7810db48360155a141e30841829823aa1f
Lightning Auriga <lightning.auriga@gmail.com>
2023-10-29 13:08:23 -0400

    feat: add basic implementation of genetic map interface class

---
+++ interpolate-genetic-position/genetic_map.cc
@@ -0,0 +1,256 @@
+/*!
+ \file genetic_map.cc
+ \brief implementation for reference genetic map file
+ \author Lightning Auriga
+ \copyright Released under the MIT License.
+ Copyright 2023 Lightning Auriga
+ */
+
+#include "interpolate-genetic-position/genetic_map.h"
+
+namespace igp = interpolate_genetic_position;
+
+igp::genetic_map::genetic_map() {
+  _gzinput = NULL;
+  _buffer = 0;
+  _buffer_size = 1000;
+  _ft = UNKNOWN;
+  _chr_old = "";
+  _pos_old = 0;
+  _gpos_old = 0.0;
+  _rate_old = 0.0;
+  _chr_new = "";
+  _pos_new = 0;
+  _gpos_new = 0.0;
+  _rate_new = 0.0;
+
+  _buffer = new char[_buffer_size];
+}
+igp::genetic_map::genetic_map(const genetic_map &obj) {
+  throw std::runtime_error(
+      "genetic_map: copy constructor operation is invalid for this class");
+}
+igp::genetic_map::~genetic_map() throw() { close(); }
+void igp::genetic_map::open(const std::string &filename, format_type ft) {
+  _ft = ft;
+  // function must chomp the expected header line of bolt format files
+  if (filename.rfind(".gz") == filename.size() - 3) {
+    _gzinput = gzopen(filename.c_str(), "rb");
+    if (!_gzinput) {
+      throw std::runtime_error("genetic_map: cannot read file \"" + filename +
+                               "\"");
+    }
+    if (_ft == BOLT) {
+      gzgets(_gzinput, _buffer, _buffer_size);
+    }
+  } else {
+    std::string line = "";
+    _input.open(filename.c_str());
+    if (!_input.is_open()) {
+      throw std::runtime_error("genetic_map: cannot read file \"" + filename +
+                               "\"");
+    }
+    if (_ft == BOLT) {
+      getline(_input, line);
+    }
+  }
+  // Load the first two values, such that a valid range is available at the
+  // beginning of iteration.
+  get();
+  get();
+}
+void igp::genetic_map::open(const char *filename, format_type ft) {
+  open(std::string(filename), ft);
+}
+bool igp::genetic_map::get() {
+  _chr_old = _chr_new;
+  _pos_old = _pos_new;
+  _gpos_old = _gpos_new;
+  _rate_old = _rate_new;
+  std::string line = "", pos1 = "", pos2 = "", gpos = "", rate = "";
+  if (_input.is_open()) {
+    if (_input.peek() == EOF) {
+      return false;
+    }
+    getline(_input, line);
+  } else {
+    if (gzgets(_gzinput, _buffer, _buffer_size) == Z_NULL) {
+      return false;
+    }
+    line = std::string(_buffer);
+  }
+  std::istringstream strm1(line);
+  if (_ft == BOLT) {
+    // BOLT format includes a first column chromosome indicator, without a "chr"
+    // prefix independent of genome build. It includes 1-22 and X encoded as 23.
+    if (!(strm1 >> _chr_new >> pos1 >> rate >> gpos)) {
+      throw std::runtime_error(
+          "genetic_map::get: cannot parse BOLT ratefile line \"" + line + "\"");
+    }
+    _pos_new = pos1;
+    _gpos_new = gpos;
+    _rate_new = rate;
+  } else if (_ft == UCSC) {
+    // UCSC format includes rate but not genetic position itself. Assuming the
+    // first position in a rate file is 0 genetic position, as is conventional,
+    // we need to track the accumulated genetic position each time we get a new
+    // entry from the file.
+    if (!(strm1 >> _chr_new >> pos1 >> pos2 >> rate)) {
+      throw std::runtime_error(
+          "genetic_map::get: cannot parse UCSC bedfile line \"" + line + "\"");
+    }
+    _pos_new = pos1;
+    _rate_new = rate;
+    _gpos_new = _gpos_old + _rate_new * (_pos_new - _pos_old);
+  } else if (_ft == UNKNOWN) {
+    throw std::runtime_error("genetic_map::get: format is unset");
+  } else {
+    throw std::runtime_error(
+        "genetic_map::get: unrecognized file format (check format_type enum)");
+  }
+  return true;
+}
+int igp::genetic_map::chromosome_to_integer(const std::string &chr) const {
+  int rep = 0;
+  std::string stripped_chr = chr;
+  if (stripped_chr.find("chr") == 0) {
+    stripped_chr = stripped_chr.substr(3);
+  }
+  if (!stripped_chr.compare("X")) {
+    rep = 23;
+  } else if (!stripped_chr.compare("Y")) {
+    rep = 24;
+  } else if (!stripped_chr.compare("MT") || !stripped_chr.compare("M")) {
+    rep = 26;
+  } else {
+    std::istringstream strm1(stripped_chr);
+    if (!(strm1 >> rep)) {
+      throw std::runtime_error(
+          "chromosome_to_integer: unhandled chromosome code \"" + chr + "\"");
+    }
+  }
+  return rep;
+}
+igp::direction igp::genetic_map::chromosome_compare(
+    const std::string &chr1, const std::string &chr2) const {
+  unsigned int1 = chromosome_to_integer(chr1);
+  unsigned int2 = chromosome_to_integer(chr2);
+  if (int1 == int2) return EQUAL;
+  return int1 < int2 ? LESS_THAN : GREATER_THAN;
+}
+void igp::genetic_map::query(const std::string &chr_query,
+                             const mpz_class &pos_query,
+                             mpf_class *gpos_interpolated) {
+  // This comparison must cautiously detect whether a variant falls before,
+  // after, or within the interval indicated by the currently loaded set of
+  // genetic positions. Certain permutations of this comparison will indicate
+  // that sorting assumptions on input data have been violated. Additionally,
+  // the test for chromosome equality must be performed carefully to deal with
+  // the many permutations of chromosome codes that are encountered in datasets.
+  direction query_vs_old = EQUAL;
+  direction query_vs_new = EQUAL;
+  while (!eof()) {
+    query_vs_old = chromosome_compare(chr_query, _chr_old);
+    query_vs_new = chromosome_compare(chr_query, _chr_new);
+    if (query_vs_old == EQUAL && query_vs_new == EQUAL) {
+      // proceed to position comparison
+      int query_pos_vs_old = cmp(pos_query, _pos_old);
+      int query_pos_vs_new = cmp(pos_query, _pos_new);
+      if (query_pos_vs_old == 0 && query_pos_vs_new == -1) {
+        // exact lower boundary rate
+        *gpos_interpolated = _gpos_old;
+        return;
+      } else if (query_pos_vs_old == 1 && query_pos_vs_new == 0) {
+        // exact upper boundary rate
+        *gpos_interpolated = _gpos_new;
+        return;
+      } else if (query_pos_vs_old == -1 && query_pos_vs_new == -1) {
+        // beginning of chromosome
+        *gpos_interpolated = 0.0;
+        return;
+      } else if (query_pos_vs_old == 1 && query_pos_vs_new == -1) {
+        // interpolate
+        *gpos_interpolated = _gpos_old + (_pos_new - _pos_old) * _rate_old;
+        return;
+      } else if (query_pos_vs_old == 1 && query_pos_vs_new == 1) {
+        // increment
+        get();
+      } else {
+        // impossible condition
+        throw std::runtime_error(
+            "genetic_map::query: an impossible condition was encountered. This "
+            "likely means that your input queries or genetic map are unsorted. "
+            "For now, the only solution to this issue is to sort your input "
+            "data; in the future, there may be an option to have the program "
+            "handle this for you, at the cost of RAM.");
+      }
+    } else if (query_vs_old == EQUAL && query_vs_new == LESS_THAN) {
+      // gpos extension beyond end of range
+      *gpos_interpolated = _gpos_old;
+      return;
+    } else if (query_vs_new == LESS_THAN) {
+      // no estimate for relevant chromosome; set to 0?
+      *gpos_interpolated = 0.0;
+      return;
+    } else if (query_vs_old == GREATER_THAN) {
+      get();
+      if (query_vs_new == GREATER_THAN) {
+        get();
+      }
+    } else {
+      // sort error
+      throw std::runtime_error(
+          "genetic_map::query: an impossible condition was encountered. This "
+          "likely means that your input queries or genetic map are unsorted. "
+          "For now, the only solution to this issue is to sort your input "
+          "data; in the future, there may be an option to have the program "
+          "handle this for you, at the cost of RAM.");
+    }
+  }
+  if (eof()) {
+    if (query_vs_new == EQUAL) {
+      // For variants at the end of the chromosome's rate information, what is
+      // the correct course of action? This current value sets out-of-range
+      // recombination to 0; this may be swapped out for linear interpolation
+      // using the last known good rate of the chromosome, which might be more
+      // biological but violates the traditional convention of analyses
+      // extending beyond a model's estimation range.
+      *gpos_interpolated = _gpos_new;
+      return;
+    } else {
+      // We may eventually want to let the user specify that this should fall
+      // back to the naive estimator.
+      *gpos_interpolated = 0;
+      return;
+    }
+  } else {
+    throw std::runtime_error(
+        "genetic_map::query: an impossible condition was encountered. This "
+        "likely means that your input queries or genetic map are unsorted. "
+        "For now, the only solution to this issue is to sort your input "
+        "data; in the future, there may be an option to have the program "
+        "handle this for you, at the cost of RAM.");
+  }
+}
+void igp::genetic_map::close() {
+  if (_input.is_open()) {
+    _input.close();
+  }
+  if (_gzinput) {
+    gzclose(_gzinput);
+    _gzinput = 0;
+  }
+  if (_buffer) {
+    delete[] _buffer;
+    _buffer = 0;
+  }
+}
+bool igp::genetic_map::eof() {
+  if (_input.is_open()) {
+    return _input.peek() == EOF;
+  }
+  if (_gzinput) {
+    return gzeof(_gzinput);
+  }
+  return false;
+}
---
+++ interpolate-genetic-position/genetic_map.h
@@ -0,0 +1,131 @@
+/*!
+ \file genetic_map.h
+ \brief interface for reference genetic map file
+ \author Lightning Auriga
+ \copyright Released under the MIT License.
+ Copyright 2023 Lightning Auriga
+ */
+
+#ifndef INTERPOLATE_GENETIC_POSITION_GENETIC_MAP_H_
+#define INTERPOLATE_GENETIC_POSITION_GENETIC_MAP_H_
+
+#include <gmpxx.h>
+#include <zlib.h>
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace interpolate_genetic_position {
+typedef enum { UNKNOWN, BOLT, UCSC } format_type;
+typedef enum { LESS_THAN, EQUAL, GREATER_THAN } direction;
+/*!
+ * \class genetic_map
+ * \brief interface class for reading data from a genetic map file.
+ * attempts to buffer data to prevent excessive memory allocation,
+ * though in the initial implementation at least, this forces input
+ * query variants to be appropriately sorted.
+ */
+class genetic_map {
+ public:
+  /*!
+   * \brief default constructor
+   */
+  genetic_map();
+  /*!
+   * \brief copy constructor
+   *
+   * Copy constructor is disabled due to internal
+   * file handle.
+   */
+  genetic_map(const genetic_map &obj);
+  /*!
+   * \brief destructor
+   */
+  ~genetic_map() throw();
+  /*!
+   * \brief initialize file connection from string
+   * \param filename name of input file
+   * \param ft descriptor of recombination rate file format
+   */
+  void open(const std::string &filename, format_type ft);
+  /*!
+   * \brief initialize file connection from char
+   * \param filename name of input file
+   * \param ft descriptor of recombination rate file format
+   * \note included for thematic compatibility with std::ifstream
+   */
+  void open(const char *filename, format_type ft);
+  /*!
+   * \brief get the next genetic map entry from file and
+   * store it in internal buffer
+   * \return boolean indicating whether a new file entry
+   * was successfully loaded. FALSE should indicate EOF.
+   */
+  bool get();
+  /*!
+   * \brief query the currently loaded data points to try
+   * to interpolate genetic position. this action may or may
+   * not cause the object to update its internal cache.
+   * \param chr_query chromosome of query variant as string
+   * \param pos_query physical position of query variant,
+   * represented as mpz
+   * \param gpos_interpolated pointer to mpf with interpolated
+   * genetic position for the query
+   */
+  void query(const std::string &chr_query, const mpz_class &pos_query,
+             mpf_class *gpos_interpolated);
+  /*!
+   * \brief close any input connection
+   */
+  void close();
+  /*!
+   * \brief probe input connection to determine whether EOF has been reached
+   * \return whether an open connection is EOF
+   *
+   * If no connection is established... not really sure.
+   */
+  bool eof();
+  /*!
+   * \brief translate an assortment of chromosome representations
+   * into simple integers for sort comparison.
+   * \param chr chromosome code, as string, for conversion
+   * \return input chromosome, as an integer on {[1,24], 26}
+   *
+   * This function should handle optional "chr" prefixes depending on input
+   * build. Chromosomes 1-22 are labeled as normal; X is interpreted as 23; Y is
+   * interpreted as 24; M or MT is interpreted as 26. XY/25, the PAR regions,
+   * are not currently handled.
+   */
+  int chromosome_to_integer(const std::string &chr) const;
+  /*!
+   * \brief compare two chromosome representations to get a consistent
+   * directionality of their comparison. Will strip prefixes and convert
+   * most standard chromosome aliases.
+   * \param chr1 first chromosome code to compare
+   * \param chr2 second chromosome code to compare
+   * \return the relationship of the first chromosome code to the second
+   */
+  direction chromosome_compare(const std::string &chr1,
+                               const std::string &chr2) const;
+
+ private:
+  std::ifstream _input;   //!< file connection for plaintext input
+  gzFile _gzinput;        //!< file connection for gzipped input
+  char *_buffer;          //!< character buffer for gzinput line reading
+  unsigned _buffer_size;  //!< size of gzinput buffer, in bytes
+  format_type _ft;        //!< stored format of input filestream
+  std::string _chr_old;   //!< chromosome of previous entry
+  std::string _chr_new;   //!< chromosome of new entry
+  mpz_class _pos_old;     //!< physical position of previous entry
+  mpz_class _pos_new;     //!< physical position of new entry
+  mpf_class _gpos_old;    //!< genetic position of previous entry
+  mpf_class _gpos_new;    //!< genetic position of new entry
+  mpf_class _rate_old;    //!< point recombination rate change of previous entry
+  mpf_class _rate_new;    //!< point recombination rate change of new entry
+};
+}  // namespace interpolate_genetic_position
+
+#endif  // INTERPOLATE_GENETIC_POSITION_GENETIC_MAP_H_
