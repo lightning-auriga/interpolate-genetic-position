3e93a873dd736173d9f12fdce6dd72eeb6631b7f
Lightning Auriga <lightning.auriga@gmail.com>
2023-11-02 18:05:08 -0400

    test: add two boundary condition tests for genetic map query

--- unit_tests/input_genetic_map_file_test.cc
+++ unit_tests/input_genetic_map_file_test.cc
@@ -40,3 +40,35 @@ TEST(input_genetic_map_fileTest, copy_constructor_disabled) {
   EXPECT_CALL(mockfile, close()).Times(AnyNumber());
   EXPECT_THROW(igp::genetic_map gm2(gm), std::runtime_error);
 }
+
+TEST(input_genetic_map_fileTest, query_beyond_end_of_chromosome) {
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(1).WillOnce(Return(false));
+  EXPECT_CALL(mockfile, get_chr_lower_bound()).Times(1).WillOnce(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound()).Times(1).WillOnce(Return("2"));
+  EXPECT_CALL(mockfile, get_gpos_lower_bound())
+      .Times(1)
+      .WillOnce(Return(mpf_class(23.4)));
+  mpf_class gpos_interpolated;
+  std::string query_chr = "1";
+  mpz_class query_pos = 1000000;
+  bool verbose = false;
+  gm.query(query_chr, query_pos, verbose, &gpos_interpolated);
+  EXPECT_EQ(gpos_interpolated, mpf_class(23.4));
+}
+
+TEST(input_genetic_map_fileTest, query_beyond_end_of_file) {
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(2).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_upper_bound()).Times(1).WillOnce(Return("23"));
+  mpf_class gpos_interpolated;
+  std::string query_chr = "M";
+  mpz_class query_pos = 1000000;
+  bool verbose = false;
+  gm.query(query_chr, query_pos, verbose, &gpos_interpolated);
+  EXPECT_EQ(gpos_interpolated, mpf_class(0.0));
+}
