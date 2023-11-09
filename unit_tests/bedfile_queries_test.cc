c22bbf9804f4aa65d9b0195456d6a92620fc4de0
Lightning Auriga <lightning.auriga@gmail.com>
2023-11-09 10:16:51 -0500

    test: add test for intersection partially upstream of genetic map

--- unit_tests/bedfile_queries_test.cc
+++ unit_tests/bedfile_queries_test.cc
@@ -16,6 +16,7 @@ using ::testing::Return;
 
 namespace igp = interpolate_genetic_position;
 
+// straightforward partial intersection
 TEST_F(geneticMapTest, bedfileQuery1) {
   igp::mock_input_genetic_map_file mockfile;
   igp::genetic_map gm(&mockfile);
@@ -57,4 +58,47 @@ TEST_F(geneticMapTest, bedfileQuery1) {
   EXPECT_EQ(result.get_rate(), mpf_class(3.32));
   EXPECT_EQ(cmp(abs(result.get_gpos() - mpf_class(3.16)), _mpf_error_tolerance),
             -1);
+}
+
+// first part of the query precedes the estimation range
+TEST_F(geneticMapTest, bedfileQuery2) {
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(1).WillOnce(Return(false));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_startpos_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(500000));
+  EXPECT_CALL(mockfile, get_endpos_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(1500000));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(1500000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(2500000));
+  EXPECT_CALL(mockfile, get_gpos_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class(1.5)));
+  EXPECT_CALL(mockfile, get_rate_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class(3.32)));
+  igp::query_result result;
+  std::string query_chr = "1";
+  mpz_class query_startpos = 250000;
+  mpz_class query_endpos = 750000;
+  bool verbose = false;
+  gm.query(query_chr, query_startpos, query_endpos, verbose, &result);
+  EXPECT_EQ(result.get_chr(), "1");
+  EXPECT_EQ(result.get_startpos(), mpz_class(250000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(500000));
+  EXPECT_EQ(result.get_rate(), mpf_class(0.0));
+  EXPECT_EQ(result.get_gpos(), mpf_class(0.0));
 }
