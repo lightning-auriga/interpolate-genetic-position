d031fe339a41a7d7331301c8f32922e480a5a577
Lightning Auriga <lightning.auriga@gmail.com>
2023-11-16 12:03:40 -0500

    test: add assortment of tests for extensions of the final chromosome of the genetic map

--- unit_tests/genetic_map_test.cc
+++ unit_tests/genetic_map_test.cc
@@ -406,3 +406,229 @@ TEST_F(geneticMapTest, queryIncrementGeneticMapWhenExceedsUpperChromosome) {
   gm.query(query_chr, query_pos, -1, verbose, &result);
   EXPECT_EQ(result.get_gpos(), mpf_class("0.0"));
 }
+
+TEST_F(geneticMapTest, queryChromMatchEndOfBoltStyleMap) {
+  // if the query is exactly the same as the final entry
+  // in the genetic map, it should return as usual
+  // when there's a direct collision; but in the implementation,
+  // the logic is separate and needs separate testing.
+  // In the instance that the genetic map is bolt-style, without
+  // an end position per estimate, the rate is guaranteed to be 0
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(AnyNumber()).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("2"));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(200000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(-1));
+  EXPECT_CALL(mockfile, get_gpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("234.5")));
+  EXPECT_CALL(mockfile, get_rate_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("0.0")));
+  igp::query_result result;
+  std::string query_chr = "2";
+  mpz_class query_pos = 200000;
+  bool verbose = false;
+  gm.query(query_chr, query_pos, -1, verbose, &result);
+  EXPECT_EQ(result.get_startpos(), mpz_class(200000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(-1));
+  EXPECT_EQ(result.get_gpos(), mpf_class("234.5"));
+  EXPECT_EQ(result.get_rate(), mpf_class("0.0"));
+}
+
+TEST_F(geneticMapTest, querySameChromBeyondEndOfBedgraphStyleMap) {
+  // if the query is exactly the same as the final entry
+  // in the genetic map, it should return as usual
+  // when there's a direct collision; but in the implementation,
+  // the logic is separate and needs separate testing.
+  // In the instance that the genetic map is bedgraph/bigwig-style,
+  // with an end position per estimate, the rate is probably nonzero
+  // and some amount of interpolation will be required.
+  // For queries beyond that end position, the effective rate
+  // must be set to 0.
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(AnyNumber()).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("2"));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(200000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(300000));
+  EXPECT_CALL(mockfile, get_gpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("234.5")));
+  EXPECT_CALL(mockfile, get_rate_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("20.1")));
+  igp::query_result result;
+  std::string query_chr = "2";
+  mpz_class query_pos = 400000;
+  bool verbose = false;
+  gm.query(query_chr, query_pos, -1, verbose, &result);
+  EXPECT_EQ(result.get_startpos(), mpz_class(400000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(-1));
+  mpf_class gpos_expected("236.51");
+  EXPECT_EQ(cmp(abs(result.get_gpos() - gpos_expected), _mpf_error_tolerance),
+            -1);
+  EXPECT_EQ(result.get_rate(), mpf_class("0.0"));
+}
+
+TEST_F(geneticMapTest, querySameChromWithinEndOfBedgraphStyleMap) {
+  // if the query is exactly the same as the final entry
+  // in the genetic map, it should return as usual
+  // when there's a direct collision; but in the implementation,
+  // the logic is separate and needs separate testing.
+  // In the instance that the genetic map is bedgraph/bigwig-style,
+  // with an end position per estimate, the rate is probably nonzero
+  // and some amount of interpolation will be required.
+  // For queries within the final range, the effective rate
+  // is whatever is set for the final range.
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(AnyNumber()).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("2"));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(200000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(300000));
+  EXPECT_CALL(mockfile, get_gpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("234.5")));
+  EXPECT_CALL(mockfile, get_rate_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("20.1")));
+  igp::query_result result;
+  std::string query_chr = "2";
+  mpz_class query_pos = 250000;
+  bool verbose = false;
+  gm.query(query_chr, query_pos, -1, verbose, &result);
+  EXPECT_EQ(result.get_startpos(), mpz_class(250000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(-1));
+  mpf_class gpos_expected("235.505");
+  EXPECT_EQ(cmp(abs(result.get_gpos() - gpos_expected), _mpf_error_tolerance),
+            -1);
+  EXPECT_EQ(result.get_rate(), mpf_class("20.1"));
+}
+
+TEST_F(geneticMapTest,
+       querySameChromWithinEndOfBedgraphStyleMapContainedRegion) {
+  // if the query is exactly the same as the final entry
+  // in the genetic map, it should return as usual
+  // when there's a direct collision; but in the implementation,
+  // the logic is separate and needs separate testing.
+  // In the instance that the genetic map is bedgraph/bigwig-style,
+  // with an end position per estimate, the rate is probably nonzero
+  // and some amount of interpolation will be required.
+  // For queries within the final range, the effective rate
+  // is whatever is set for the final range.
+  // Query is region contained within final map interval.
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(AnyNumber()).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("2"));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(200000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(300000));
+  EXPECT_CALL(mockfile, get_gpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("234.5")));
+  EXPECT_CALL(mockfile, get_rate_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("20.1")));
+  igp::query_result result;
+  std::string query_chr = "2";
+  mpz_class query_startpos = 250000;
+  mpz_class query_endpos = 275000;
+  bool verbose = false;
+  gm.query(query_chr, query_startpos, query_endpos, verbose, &result);
+  EXPECT_EQ(result.get_startpos(), mpz_class(250000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(275000));
+  mpf_class gpos_expected("235.505");
+  EXPECT_EQ(cmp(abs(result.get_gpos() - gpos_expected), _mpf_error_tolerance),
+            -1);
+  EXPECT_EQ(result.get_rate(), mpf_class("20.1"));
+}
+
+TEST_F(geneticMapTest,
+       querySameChromOverlappingEndOfBedgraphStyleMapContainedRegion) {
+  // if the query is exactly the same as the final entry
+  // in the genetic map, it should return as usual
+  // when there's a direct collision; but in the implementation,
+  // the logic is separate and needs separate testing.
+  // In the instance that the genetic map is bedgraph/bigwig-style,
+  // with an end position per estimate, the rate is probably nonzero
+  // and some amount of interpolation will be required.
+  // For queries within the final range, the effective rate
+  // is whatever is set for the final range.
+  // Query is region overlapping endpoint of final map interval.
+  igp::mock_input_genetic_map_file mockfile;
+  igp::genetic_map gm(&mockfile);
+  EXPECT_CALL(mockfile, close()).Times(AnyNumber());
+  EXPECT_CALL(mockfile, eof()).Times(AnyNumber()).WillRepeatedly(Return(true));
+  EXPECT_CALL(mockfile, get_chr_lower_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("1"));
+  EXPECT_CALL(mockfile, get_chr_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return("2"));
+  EXPECT_CALL(mockfile, get_startpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(200000));
+  EXPECT_CALL(mockfile, get_endpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(300000));
+  EXPECT_CALL(mockfile, get_gpos_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("234.5")));
+  EXPECT_CALL(mockfile, get_rate_upper_bound())
+      .Times(AnyNumber())
+      .WillRepeatedly(Return(mpf_class("20.1")));
+  igp::query_result result;
+  std::string query_chr = "2";
+  mpz_class query_startpos = 250000;
+  mpz_class query_endpos = 350000;
+  bool verbose = false;
+  gm.query(query_chr, query_startpos, query_endpos, verbose, &result);
+  EXPECT_EQ(result.get_startpos(), mpz_class(250000));
+  EXPECT_EQ(result.get_endpos(), mpz_class(300000));
+  mpf_class gpos_expected("235.505");
+  EXPECT_EQ(cmp(abs(result.get_gpos() - gpos_expected), _mpf_error_tolerance),
+            -1);
+  EXPECT_EQ(result.get_rate(), mpf_class("20.1"));
+}
